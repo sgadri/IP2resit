65e166abcdab0098b78c5b58f9ced60732432a03
sgadri <sgadri16@alustudent.com>
2021-01-05 20:36:10 +0100

    ip2 reist

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8, -187.3),
+    mapTypeId: "terrain",
+  });
+  
+  const script = document.createElement("script");
+
+  script.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+
+const eqfeed_callback = function (results) {
+  for (let i = 0; i < results.features.length; i++) {
+    const coords = results.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
+  }
+};
+
